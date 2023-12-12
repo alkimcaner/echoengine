610c7752da9dd1ac0228938a070f592a767cac65
Alkım Caner Soydan <alkimcanersoydan@gmail.com>
2023-12-12 23:38:20 +0300

    feat: add huggingface api

---
+++ src/app/api/completion/route.ts
@@ -0,0 +1,29 @@
+import { HfInference } from "@huggingface/inference";
+import { HuggingFaceStream, StreamingTextResponse } from "ai";
+
+// Create a new Hugging Face Inference instance
+const Hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  // Extract the `prompt` from the body of the request
+  const { prompt } = await req.json();
+
+  const response = Hf.textGenerationStream({
+    model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+    inputs: `<|prompter|>${prompt}<|endoftext|><|assistant|>`,
+    parameters: {
+      max_new_tokens: 200,
+      repetition_penalty: 1,
+      return_full_text: false,
+    },
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = HuggingFaceStream(response);
+
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
