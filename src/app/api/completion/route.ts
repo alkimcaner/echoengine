611f7235591c54288052728943f11af384dddc45
Alkım Caner Soydan <alkimcanersoydan@gmail.com>
2023-12-13 00:15:17 +0300

    refactor: change prompt template

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -13,9 +13,9 @@ export async function POST(req: Request) {
 
   const response = Hf.textGenerationStream({
     model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
-    inputs: `<|prompter|>${prompt}<|endoftext|><|assistant|>`,
+    inputs: `[INST] ${prompt} [/INST]`,
     parameters: {
-      max_new_tokens: 200,
+      max_new_tokens: 300,
       repetition_penalty: 1,
       return_full_text: false,
     },
